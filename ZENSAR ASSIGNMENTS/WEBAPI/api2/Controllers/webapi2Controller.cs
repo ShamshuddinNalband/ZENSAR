4cd2b62e6b2ce80dab3d573df07781343a1fd43d
ShamshuddinNalband <98321199+ShamshuddinNalband@users.noreply.github.com>
2022-03-21 09:44:36 +0530

    Add files via upload

---
+++ ZENSAR ASSIGNMENTS/WEBAPI/api2/Controllers/webapi2Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webapi2.Models;
+
+namespace webapi2.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IHttpActionResult GetEmp(int id)
+        {
+            ZensarDBEntities ze = new ZensarDBEntities();
+            var result = ze.GetSalaryById(id).ToList();
+            return Ok(result);
+        }
+    }
+}
